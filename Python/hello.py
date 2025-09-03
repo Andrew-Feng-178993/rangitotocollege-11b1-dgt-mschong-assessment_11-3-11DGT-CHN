647600f4a6ae0b0259739b6d35c631aa043956ac
Andrew-Feng-178993 <178993@cloud.rangitoto.school.nz>
2025-09-03 14:55:53 +1200

    this is my test for tkinter

--- Python/hello.py
+++ Python/hello.py
@@ -1,7 +1,22 @@
-print("Hello world!")
-print("hello")
-print("I like pizza")
-print("I like ice cream")
-print("I like chocolate")
-print("I like cookies")
-print("I like cake")
+"""from tkinter import *
+top = Tk()
+Lb = Listbox(top)
+Lb.insert(1, 'Python')
+Lb.insert(2, 'Java')
+Lb.insert(3, 'C++')
+Lb.insert(4, 'Any other')
+Lb.insert(5, 'Javadoodle')
+Lb.pack()
+top.mainloop()"""
+
+from tkinter import *
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+mylist = Listbox(root, yscrollcommand=scrollbar.set)
+
+for line in range(100):
+    mylist.insert(END, 'This is line number' + str(line))
+mylist.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=mylist.yview)
+mainloop()
