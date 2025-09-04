bf96a5f55f0f8a6af990fda7e01bfcb469b9e1ac
Andrew-Feng-178993 <178993@cloud.rangitoto.school.nz>
2025-09-05 09:32:14 +1200

    Snake game testing?

--- Python/hello.py
+++ Python/hello.py
@@ -1,22 +1,79 @@
-"""from tkinter import *
-top = Tk()
-Lb = Listbox(top)
-Lb.insert(1, 'Python')
-Lb.insert(2, 'Java')
-Lb.insert(3, 'C++')
-Lb.insert(4, 'Any other')
-Lb.insert(5, 'Javadoodle')
-Lb.pack()
-top.mainloop()"""
-
 from tkinter import *
-root = Tk()
-scrollbar = Scrollbar(root)
-scrollbar.pack(side=RIGHT, fill=Y)
-mylist = Listbox(root, yscrollcommand=scrollbar.set)
+import random
 
-for line in range(100):
-    mylist.insert(END, 'This is line number' + str(line))
-mylist.pack(side=LEFT, fill=BOTH)
-scrollbar.config(command=mylist.yview)
-mainloop()
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+SPEED = 100
+SPACE_SIZE = 20
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
+
+
+class Snake:
+    
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square) 
+
+
+class Food:
+    
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+
+        self.coordinate = [x, y]
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Andrew's Snake Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down' 
+
+label = Label(window, text="Score: {}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+
+window.mainloop()
