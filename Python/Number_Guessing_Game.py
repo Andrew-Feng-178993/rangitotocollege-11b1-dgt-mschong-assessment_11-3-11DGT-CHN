f29f80cae8cc64f4cbc794ac0a44f3b774714e67
Andrew-Feng-178993 <178993@cloud.rangitoto.school.nz>
2025-09-08 10:49:18 +1200

    New number guessing game?

--- Python/Number_Guessing_Game.py
+++ Python/Number_Guessing_Game.py
@@ -1,5 +1,6 @@
 import random
 
+print("WELCOME TO ANDREW'S GUESSING GAME!!!")
 guesses_num = []
 
 easy_level_num = 100
@@ -7,23 +8,24 @@ medium_level_num = 1000
 hard_level_num = 10000
 extra_hard_level_num = 100000
 
-level = input("Pick a level of difficulty: Easy, Medium, Hard, Extra Hard.\n")
-
-if level == "Easy":
-    random_num = random.randint(1, easy_level_num)
-    print(random_num)
-elif level == "Medium":
-    random_num = random.randint(1, medium_level_num)
-    print(random_num)
-elif level == "Hard":
-    random_num = random.randint(1, hard_level_num)
-    print(random_num)
-elif level == "Extra Hard":
-    random_num = random.randint(1, extra_hard_level_num)
-    print(random_num)
-else:
-    print("That is not a valid level! Please pick again.")
+while True:
     level = input("Pick a level of difficulty: Easy, Medium, Hard, Extra Hard.\n")
+
+    if level == "Easy":
+        random_num = random.randint(1, easy_level_num)
+        print(random_num)
+    elif level == "Medium":
+        random_num = random.randint(1, medium_level_num)
+        print(random_num)
+    elif level == "Hard":
+        random_num = random.randint(1, hard_level_num)
+        print(random_num)
+    elif level == "Extra Hard":
+        random_num = random.randint(1, extra_hard_level_num)
+        print(random_num)
+    else:
+        print("That is not a valid level! Please pick again.")
+   
 
 
 
