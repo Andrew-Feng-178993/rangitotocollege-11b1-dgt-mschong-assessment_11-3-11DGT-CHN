7ba12daf6c71773f6481d6ca16e05240e2d54167
Andrew-Feng-178993 <178993@cloud.rangitoto.school.nz>
2025-09-08 11:03:44 +1200

    LAst time hopefully?

--- Python/Number_Guessing_Game.py
+++ Python/Number_Guessing_Game.py
@@ -1,7 +1,7 @@
 import random
 
 print("WELCOME TO ANDREW'S GUESSING GAME!!!")
-guesses_num = []
+list = []
 
 easy_level_num = 100
 medium_level_num = 1000
@@ -14,33 +14,46 @@ while True:
     if level == "Easy":
         random_num = random.randint(1, easy_level_num)
         print(random_num)
+        
+        break
     elif level == "Medium":
         random_num = random.randint(1, medium_level_num)
         print(random_num)
+      
+        break
     elif level == "Hard":
         random_num = random.randint(1, hard_level_num)
         print(random_num)
+   
+        break
     elif level == "Extra Hard":
         random_num = random.randint(1, extra_hard_level_num)
         print(random_num)
+   
+        break
     else:
         print("That is not a valid level! Please pick again.")
    
-
-
-
-guess = int(input("Please guess a number"))
+guess = int(input("Please guess a number:\n"))
 while True:
-    if guess < 1 or guess > easy_level_num:
-        print("That is not within the valid range!")
-    elif guess == random_num:
-        list.append("lol")
-        print(f"Congratulations! You guessed the number in {len(guesses_num)} guesses!")
-    elif guess < random_num:
+    ###if guess < 1 or guess > limit:
+        ###print("That is not within the valid range!")
+    if guess < random_num:
         list.append("lol")
         print("The number you are looking for is higher!")
+        guess = int(input("Please guess a number:\n"))
+        
     elif guess > random_num:
         list.append("lol")
         print("The number you are looking for is lower!")
+        guess = int(input("Please guess a number:\n"))
+        
+    elif guess == random_num:
+        list.append("lol")
+        print(f"Congratulations! You guessed the number in {len(list)} guesses!")
+        
+
+
+
 
 
