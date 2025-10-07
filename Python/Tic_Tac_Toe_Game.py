f28d344350933f723486b520dc15391b6cfaecf3
Andrew-Feng-178993 <178993@cloud.rangitoto.school.nz>
2025-10-08 10:18:34 +1300

    board for tic tac toe is not printing for some reason

---
+++ Python/Tic_Tac_Toe_Game.py
@@ -0,0 +1,31 @@
+board = { "-", "-", "-",
+          "-", "-", "-",
+          "-", "-", "-" }
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+
+def playerInput(board):
+    inp = int(input("Enter a number from 1 to 9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("A player is already in that position!")
+
+
+
+
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+
+      
